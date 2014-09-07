feaa2277c1cdf7c99d5ae9a2b82924ddf70ac0da
Stewart Henderson <info@bowlofstew.com>
2014-09-07 17:05:38 -0500

    Updated to use net lib.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,15 @@
+#include <boost/network.hpp>
+
+using namespace boost::network;
+using namespace boost::network::http;
 
 int
 main(int argc, char *argv[]) {
+    client::request req("http://www.target.com/robots.txt");
+    req << header("Connection", "close");
+    client cli;
+    client::response res = cli.get(req);
+    std::string bod = body(res);
+    std::cout << bod <<std::endl;
     return 0;
 }
